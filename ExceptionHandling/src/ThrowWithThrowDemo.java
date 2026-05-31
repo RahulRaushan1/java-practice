722168858c8a61a6fccf03c255a58e82a281cccc
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-05-31 17:19:25 +0530

    Throw and Throws combine Demo

--- ExceptionHandling/src/ThrowWithThrowDemo.java
+++ ExceptionHandling/src/ThrowWithThrowDemo.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 public class ThrowWithThrowDemo {
 
     private static void checkAge(int age) throws Exception{
-        if(age<18) throw new ArithmeticException("Age is not Eligible for Voting");
+        if(age<18) {
+            throw new ArithmeticException("Age is not Eligible for Voting");
+        }
+
+        System.out.println("Eligible");
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Your Age :");
+
+        int age = sc.nextInt();
+
         try{
-            checkAge(13);
+            checkAge(age);
         }
         catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
 }
-// Age is not Eligible for Voting
+
